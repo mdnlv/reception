5a204fcc71dcd44db626c90c7f921feccbc8ba78
mikhail <pavlovtfspb@gmail.com>
2021-07-08 12:32:12 +0300

    исправил ошибку валидации полиса

--- src/components/forms/wizards/RegCardWizard/validation.ts
+++ src/components/forms/wizards/RegCardWizard/validation.ts
@@ -30,8 +30,8 @@ const validation = Yup.object<FormikErrors<ValidationType>>().shape({
     }),
     policyOms: Yup.object({
       timeType: Yup.string().required('Не выбран тип времени полиса'),
-      from: Yup.string().required('Не задана дата начала действия полиса'),
-      to: Yup.string().required('Не задана дата окончания действия полиса'),
+      from: Yup.string().required('Не задана дата начала действия полиса').nullable(),
+      to: Yup.string().required('Не задана дата окончания действия полиса').nullable(),
       serial: Yup.string().required('Не введена серия полиса'),
       number: Yup.string().required('Не введен номер полиса'),
       cmo: Yup.string().required('Не выбрана СМО'),
