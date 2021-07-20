736d69ec5d73a3a58d48e4c23505a085a86c26f7
mikhail <pavlovtfspb@gmail.com>
2021-07-20 12:15:06 +0300

    исправил отображение вкладки "прочее"

--- src/components/forms/wizards/RegCardWizard/pages/Etc/Etc.tsx
+++ src/components/forms/wizards/RegCardWizard/pages/Etc/Etc.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import {Col, Input, Row} from 'antd';
 
-import {DROPDOWN_TITLE, LABELS} from "./types";
+import {LABELS} from "./types";
 
-import DropDownContent from '../../../../../elements/DropDownContent/DropDownContent';
 import FormField from '../../../../components/FormField/FormField';
-import FormArrayField from '../../../../components/FormArrayField/FormArrayField';
 
 const Etc: React.FC = () => {
   return (
     <form className={'wizard-step etc-form'}>
       <div className="form-section">
-        <DropDownContent title={DROPDOWN_TITLE}>
-          <FormArrayField
-            values={[]}
-            name={'items'}
-            renderChild={(key, index) => (
-              <div key={index}>
-                <Row>
-                  <Col>
-                    <FormField label={LABELS.NOTE}>
-                      <Input.TextArea cols={120} rows={10}/>
-                    </FormField>
-                  </Col>
-                </Row>
-              </div>
-            )}
-          />
-        </DropDownContent>
+        <Row>
+          <Col>
+            <FormField label={LABELS.NOTE}>
+              <Input.TextArea cols={120} rows={10}/>
+            </FormField>
+          </Col>
+        </Row>
       </div>
     </form>
   );
