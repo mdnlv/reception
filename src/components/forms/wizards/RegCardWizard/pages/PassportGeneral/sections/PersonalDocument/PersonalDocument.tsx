575ef9c81b6809921c5980d1e3af32ea2496f7da
mikhail <pavlovtfspb@gmail.com>
2021-08-05 19:08:10 +0300

    изменил отображение формы документа

--- src/components/forms/wizards/RegCardWizard/pages/PassportGeneral/sections/PersonalDocument/PersonalDocument.tsx
+++ src/components/forms/wizards/RegCardWizard/pages/PassportGeneral/sections/PersonalDocument/PersonalDocument.tsx
@@ -34,7 +34,7 @@ const PersonalDocument: FC<SectionProps> = ({
     <div className="form-section personal-document">
       <h2>Документ</h2>
       <Row gutter={16}>
-        <Col xl={24} xxl={8}>
+        <Col xl={24}>
           <FormField label={LABELS.PASSPORT} name={`${selectionValuePath}.passportType`}>
             <FastSearchSelect
               filterOption
@@ -46,6 +46,8 @@ const PersonalDocument: FC<SectionProps> = ({
             </FastSearchSelect>
           </FormField>
         </Col>
+      </Row>
+      <Row gutter={16}>
         <Col span={2}>
           <FormField label={LABELS.SERIAL} name={`${selectionValuePath}.serialFirst`}>
             <FastInput name={`${selectionValuePath}.serialFirst`} />
@@ -61,7 +63,7 @@ const PersonalDocument: FC<SectionProps> = ({
             <FastInput name={`${selectionValuePath}.number`} />
           </FormField>
         </Col>
-        <Col xl={7} xxl={4}>
+        <Col xl={7} xxl={7}>
           <FormField label={LABELS.GIVEN_DATE} name={`${selectionValuePath}.fromDate`}>
             <FastDatePicker
               name={`${selectionValuePath}.fromDate`}
